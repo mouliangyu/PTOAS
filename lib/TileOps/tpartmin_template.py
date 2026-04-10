83d70eccd58106ff80dc1bccfb5538c13c8921e6
caojian5 <caojian5@huawei.com>
2026-04-10 18:13:43 +0800

    add part logic to template py

--- lib/TileOps/tpartmin_template.py
+++ lib/TileOps/tpartmin_template.py
@@ -11,14 +11,101 @@ import tilelang_dsl as pto
 )
 def template_tpartmin(dst: pto.Tile, src0: pto.Tile, src1: pto.Tile):
     dtype = dst.element_type
-    valid_rows, valid_cols = dst.valid_shape
+    lanes = pto.get_lanes(dtype)
 
-    for row in range(0, valid_rows, 1):
-        remained = valid_cols
-        for col in range(0, valid_cols, pto.get_lanes(dtype)):
-            mask, remained = pto.make_mask(dtype, remained)
-            lhs = pto.vlds(src0[row, col:])
-            rhs = pto.vlds(src1[row, col:])
-            result = pto.vmin(lhs, rhs, mask)
-            pto.vsts(result, dst[row, col:], mask)
+    dst_valid_rows, dst_valid_cols = dst.valid_shape
+    src0_valid_rows, src0_valid_cols = src0.valid_shape
+    src1_valid_rows, src1_valid_cols = src1.valid_shape
+
+    src0_eq_dst = (src0_valid_rows == dst_valid_rows and src0_valid_cols == dst_valid_cols)
+    src1_eq_dst = (src1_valid_rows == dst_valid_rows and src1_valid_cols == dst_valid_cols)
+
+    if src0_eq_dst:
+        if src1_eq_dst:
+            for row in range(0, dst_valid_rows, 1):
+                remained = dst_valid_cols
+                for col in range(0, dst_valid_cols, lanes):
+                    mask, remained = pto.make_mask(dtype, remained)
+                    lhs = pto.vlds(src0[row, col:])
+                    rhs = pto.vlds(src1[row, col:])
+                    summed = pto.vmin(lhs, rhs, mask)
+                    pto.vsts(summed, dst[row, col:], mask)
+        else:
+            src1_row_lt_dst = (src1_valid_rows < dst_valid_rows and src1_valid_cols == dst_valid_cols)
+            src1_col_lt_dst = (src1_valid_rows <= dst_valid_rows and src1_valid_cols < dst_valid_cols)
+            if src1_row_lt_dst:
+                if src1_valid_cols > 0:
+                    for row in range(0, src1_valid_rows, 1):
+                        remained = src1_valid_cols
+                        for col in range(0, src1_valid_cols, lanes):
+                            mask, remained = pto.make_mask(dtype, remained)
+                            lhs = pto.vlds(src0[row, col:])
+                            rhs = pto.vlds(src1[row, col:])
+                            summed = pto.vmin(lhs, rhs, mask)
+                            pto.vsts(summed, dst[row, col:], mask)
+
+                for row in range(src1_valid_rows, dst_valid_rows, 1):
+                    remained = dst_valid_cols
+                    for col in range(0, dst_valid_cols, lanes):
+                        mask, remained = pto.make_mask(dtype, remained)
+                        val = pto.vlds(src0[row, col:])
+                        pto.vsts(val, dst[row, col:], mask)
+
+            if src1_col_lt_dst:
+                for row in range(0, dst_valid_rows, 1):
+                    remained = dst_valid_cols
+                    for col in range(0, dst_valid_cols, lanes):
+                        mask, remained = pto.make_mask(dtype, remained)
+                        val = pto.vlds(src0[row, col:])
+                        pto.vsts(val, dst[row, col:], mask)
+
+                if src1_valid_cols > 0:
+                    for row in range(0, src1_valid_rows, 1):
+                        remained = src1_valid_cols
+                        for col in range(0, src1_valid_cols, lanes):
+                            mask, remained = pto.make_mask(dtype, remained)
+                            lhs = pto.vlds(src0[row, col:])
+                            rhs = pto.vlds(src1[row, col:])
+                            summed = pto.vmin(lhs, rhs, mask)
+                            pto.vsts(summed, dst[row, col:], mask)
+    elif src1_eq_dst:
+        src0_row_lt_dst = (src0_valid_rows < dst_valid_rows and src0_valid_cols == dst_valid_cols)
+        src0_col_lt_dst = (src0_valid_rows <= dst_valid_rows and src0_valid_cols < dst_valid_cols)
+
+        if src0_row_lt_dst:
+            if src0_valid_rows > 0:
+                for row in range(0, src0_valid_rows, 1):
+                    remained = src0_valid_cols
+                    for col in range(0, src0_valid_cols, lanes):
+                        mask, remained = pto.make_mask(dtype, remained)
+                        lhs = pto.vlds(src0[row, col:])
+                        rhs = pto.vlds(src1[row, col:])
+                        summed = pto.vmin(lhs, rhs, mask)
+                        pto.vsts(summed, dst[row, col:], mask)
+
+            for row in range(src0_valid_rows, dst_valid_rows, 1):
+                remained = dst_valid_cols
+                for col in range(0, dst_valid_cols, lanes):
+                    mask, remained = pto.make_mask(dtype, remained)
+                    val = pto.vlds(src1[row, col:])
+                    pto.vsts(val, dst[row, col:], mask)
+
+        if src0_col_lt_dst:
+            for row in range(0, dst_valid_rows, 1):
+                remained = dst_valid_cols
+                for col in range(0, dst_valid_cols, lanes):
+                    mask, remained = pto.make_mask(dtype, remained)
+                    val = pto.vlds(src1[row, col:])
+                    pto.vsts(val, dst[row, col:], mask)
+
+            if src0_valid_cols > 0:
+                for row in range(0, src0_valid_rows, 1):
+                    remained = src0_valid_cols
+                    for col in range(0, src0_valid_cols, lanes):
+                        mask, remained = pto.make_mask(dtype, remained)
+                        lhs = pto.vlds(src0[row, col:])
+                        rhs = pto.vlds(src1[row, col:])
+                        summed = pto.vmin(lhs, rhs, mask)
+                        pto.vsts(summed, dst[row, col:], mask)
+
     return
