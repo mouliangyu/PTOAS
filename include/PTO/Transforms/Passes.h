23f7a023a28d06dd077288b3007c2ef0020819be
mouliangyu <mouliangyu@huawei.com>
2026-04-13 00:11:29 +0800

    brough back the lost pass and pto op after rebase

--- include/PTO/Transforms/Passes.h
+++ include/PTO/Transforms/Passes.h
@@ -80,6 +80,8 @@ std::unique_ptr<Pass> createMemrefToTileBufPass();
 std::unique_ptr<Pass> createExpandTileOpPass();
 std::unique_ptr<Pass> createExpandTileOpPass(const ExpandTileOpOptions &options);
 std::unique_ptr<Pass> createFoldTileBufIntrinsicsPass();
+std::unique_ptr<Pass>
+createPTOInlineLibCallPass(const PTOInlineLibCallOptions &options = {});
 //===----------------------------------------------------------------------===//
 // Registration
 //===----------------------------------------------------------------------===//
