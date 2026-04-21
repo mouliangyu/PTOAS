953165838961f25c9e420633f309d5332f3ec793
liggest <liggest@sina.cn>
2026-04-21 15:26:52 +0800

    Add TNEG tileop template

---
+++ lib/TileOps/tneg_template.py
@@ -0,0 +1,19 @@
+import tilelang_dsl as pto
+
+
+@pto.vkernel(
+    target="a5",
+    op="pto.tneg"
+)
+def template_tneg(src: pto.Tile, dst: pto.Tile):
+    dtype = dst.element_type
+    valid_rows, valid_cols = dst.valid_shape
+
+    for row in range(0, valid_rows, 1):
+        remained = valid_cols
+        for col in range(0, valid_cols, pto.get_lanes(dtype)):
+            mask, remained = pto.make_mask(dtype, remained)
+            vinput = pto.vlds(src[row, col:])
+            result = pto.vneg(vinput, mask)
+            pto.vsts(result, dst[row, col:], mask)
+    return
