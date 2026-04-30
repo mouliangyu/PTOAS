c404f506ad451fc2b60fd2c50f323a9880297c7d
pbbb205 <625293903@qq.com>
2026-04-30 14:43:38 +0800

    add tilebuf static check

--- lib/PTO/IR/PTOAttrs.cpp
+++ lib/PTO/IR/PTOAttrs.cpp
@@ -68,7 +68,7 @@ LogicalResult TileBufConfigAttr::verify(function_ref<InFlightDiagnostic()> emitE
     return emitError() << "s_fractal_size must be i32", failure();
 
   int32_t s = (int32_t)sFractalSize.getInt();
-  f (s != 32 && s != 512 && s != 1024)
+  if (s != 32 && s != 512 && s != 1024)
     return emitError() << "unsupported s_fractal_size: " << s
                        << ", must be one of {32, 512, 1024}", failure();
 
