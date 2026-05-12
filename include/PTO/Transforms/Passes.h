a26c1e538aa9e9acc015b8e56c128f75a5436226
mouliangyu <mouliangyu@huawei.com>
2026-05-13 05:07:19 +0800

    Add VPTO MAD semantic ops

--- include/PTO/Transforms/Passes.h
+++ include/PTO/Transforms/Passes.h
@@ -67,7 +67,7 @@ std::unique_ptr<Pass> createPTOViewToMemrefPass();
 std::unique_ptr<Pass> createInferPTOLayoutPass();
 std::unique_ptr<Pass> createPTOA5NormalizeTMovPass();
 std::unique_ptr<Pass> createPTOInferVPTOVecScopePass();
-std::unique_ptr<Pass> createPTOVPTOExpandBridgeOpsPass();
+std::unique_ptr<Pass> createVPTOExpandWrapperOpsPass();
 std::unique_ptr<Pass> createPTOVPTOPtrBoundaryPass();
 std::unique_ptr<Pass> createVPTOPtrNormalizePass();
 std::unique_ptr<Pass> createVPTOPtrCastCleanupPass();
